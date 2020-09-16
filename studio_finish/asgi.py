172dbf0b2a50432761331db921080bb0d526ea96
jayab2010 <jayab2010@gmail.com>
2020-09-16 07:06:19 +0000

    initial commit

---
+++ studio_finish/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for studio_finish project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studio_finish.settings')
+
+application = get_asgi_application()
